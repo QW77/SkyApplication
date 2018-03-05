b43d871682e52105d6f8c277bb25bcbfabd750c9
QW77 <QW77@gmail.com>
2018-03-05 20:59:29 +0800

    share

--- app/src/main/java/com/sky/app/ui/activity/seller/ShopCenterActivity.java
+++ app/src/main/java/com/sky/app/ui/activity/seller/ShopCenterActivity.java
@@ -434,33 +434,23 @@ public class ShopCenterActivity extends BaseViewActivity<ShopContract.IShopCente
     private void showShare() {
 //        ShareSDK
         OnekeyShare oks = new OnekeyShare();
-        //关闭sso授权
+//        关闭sso授权
         oks.disableSSOWhenAuthorize();
-
-        // 分享时Notification的图标和文字  2.5.9以后的版本不     调用此方法
-//        oks.setNotification(R.drawable.ic_launcher,getString(R.string.app_name));
-        // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用,// title标题：微信、QQ（新浪微博不需要标题）
+//         title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用,// title标题：微信、QQ（新浪微博不需要标题）
         oks.setTitle(productIntroduceOut.getNick_name());
-
-        // text是分享文本，所有平台都需要这个字段
+//         text是分享文本，所有平台都需要这个字段
         oks.setText(productIntroduceOut.getMain_business_desc());
-        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
-//        oks.setImagePath(Environment.getExternalStorageDirectory().getPath()+"/sdcard/test.jpg");//确保SDcard下面存在此张图片
-        // url仅在微信（包括好友和朋友圈）中使用
+//         url仅在微信（包括好友和朋友圈）中使用
         oks.setUrl(Constants.Url.BASE_URL+"h5_product/un/get_user_home_products?user_id="+productIntroduceOut.getUser_id());
-        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
-//        oks.setComment("我是测试评论文本");
-        // site是分享此内容的网站名称，仅在QQ空间使用
+//         site是分享此内容的网站名称，仅在QQ空间使用
         oks.setSite("51工匠");
-        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+//         siteUrl是分享此内容的网站地址，仅在QQ空间使用
         oks.setSiteUrl(Constants.Url.BASE_URL+"h5_product/un/get_user_home_products?user_id="+productIntroduceOut.getUser_id());
-        //网络图片的url：所有平台
+//        网络图片的url：所有平台
         oks.setImageUrl(productIntroduceOut.getPic_url());//网络图片rul
-        // Url：仅在QQ空间使用
+//         Url：仅在QQ空间使用
         oks.setTitleUrl(Constants.Url.BASE_URL+"h5_product/un/get_user_home_products?user_id="+productIntroduceOut.getUser_id());  //网友点进链接后，可以看到分享的详情
-
-
-        // 启动分享GUI
+//         启动分享GUI
         oks.show(this);
     }
 }
